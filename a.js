56dee2222eeb39e153f59f7aced6617027f7647b
Huong <huongnguyenak96@gmail.com>
2019-04-13 15:31:42 +0700

    type

---
+++ a.js
@@ -0,0 +1,8 @@
+// console.log("Hello TS");
+// tsc --init
+var decimal = 10;
+var arr = [2, 4, 5];
+var arr02 = [2, 4, 5, 'sasa'];
+var newArr;
+newArr = [3, 4, 6, 'saa', true];
+// newArr
---
+++ a.ts
@@ -0,0 +1,9 @@
+// console.log("Hello TS");
+// tsc --init
+let decimal: number = 10;
+let arr : number[] = [2,4,5];
+let arr02 : (number|string)[] = [2,4,5,'sasa']
+
+let newArr: any;
+newArr = [3,4,6,'saa', true]
+// newArr
